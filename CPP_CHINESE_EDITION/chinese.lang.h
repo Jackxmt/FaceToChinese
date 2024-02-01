4eeda86794b027f72308dcbf533652f62f2c181b
Jackxmt <147888716+Jackxmt@users.noreply.github.com>
2024-02-01 19:45:41 +0800

    CPP_CHINESE_EDITION_1.0

---
+++ CPP_CHINESE_EDITION/chinese.lang.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <chrono>
+#include <cstdlib>
+#include <cstdio>
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cerrno>
+#include <clocale>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#define 定义
+#define 整数 int
+#define 代码块始 {
+#define 代码块完 }
+#define 返回 return
+#define 结束 ;
+#define 输出 cout
+#define 输出流操作符 <<
+#define 换行 endl
+#define 阴 false
+#define 阳 true
+#define 主函数 main
+#define 循环 for
+#define 括号 (
+#define 括回 )
+#define 为 =
+#define 等于 ==
+#define 自增 ++
+#define 自减 --
+#define 条件循环 while
+#define 如果 if
+#define 执行
+#define 使用 using
+#define 命名空间 namespace
+#define 标准命名空间 std
+#define 那么返回 ?
+#define 其他 :
+#define 否则 else
+#define 输入流操作符 >>
+#define 方括号 [
+#define 方括回 ]
+#define 双精度浮点数 double
+#define 单精度浮点数 float
+#define 字符 char
+#define 字符串 string
+#define 打印(a) printf(a)
+#define 与 &&
+#define 或 ||
+#define 非 !
+#if defined(__GNUC__)
+    #define 一百二十八位整数 __int128
+#endif
